bab26cefd7ecdccec985be992dc7dbc205717738
Lilia <Lilia@BB-0032>
2023-12-27 14:37:51 +0200

    Add project files.

---
+++ OOP_Principles_3/DecimalNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Principles_3
+{
+    internal struct DecimalNumbers
+    {
+        public int num { get; set; }
+        public string binary { get; set; }
+        public string octal { get; set; }
+        public string hexa { get; set; }
+        
+        public static void BinarySystem(int num, string binary)
+        {
+            while(num!=0) 
+            { 
+                if (num%2==0)
+                {
+                    num /= 2;
+                    binary += "0";
+                }
+                else
+                {
+                    num /= 2;
+                    binary += "1";
+                }
+            
+            }
+            Console.WriteLine($"Number in binary system is {new string(binary.Reverse().ToArray())}");
+        }
+        public static void OctalSystem(int num, string octal)
+        {
+            while (num != 0)
+            {
+                octal += num % 8;
+                num /= 8;
+
+            }
+            Console.WriteLine($"Number in octal system is {new string(octal.Reverse().ToArray())}");
+
+        }
+        public static void HexadecimalSystem(int num, string hexa)
+        {
+                hexa = Convert.ToString(num,16);
+            Console.WriteLine($"Number in hexadecimal system is {hexa}");
+        }
+    }
+}
---
+++ OOP_Principles_3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP_Principles_3
+{
+    internal class Program
+    {
+        private static string binary;
+
+        private static string octal;
+
+        private static string hexa;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number!");
+            var IsNum = int.TryParse(Console.ReadLine(), out int num);
+            if (IsNum==false)
+            {
+                throw new Exception("Sorry! Try again!");
+            }
+            DecimalNumbers.BinarySystem(num,binary);
+            Console.WriteLine("\b");
+            DecimalNumbers.OctalSystem(num,octal);
+            Console.WriteLine("\b");
+            DecimalNumbers.HexadecimalSystem(num, hexa);
+        }
+    }
+}
